377e897f30ee591bd9adce76df788cf026a10fe4
vishesh-baj <vishesh.bajpayee@icloud.com>
2022-03-11 18:16:58 -0600

    added overlay for navbar

--- plants-landing-page/script.js
+++ plants-landing-page/script.js
@@ -3,10 +3,13 @@
 const mobileMenuBtn = document.querySelector(".mobile-menu-btn");
 const mobileMenu = document.querySelector(".mobile-menu");
 const closeBtn = document.querySelector(".close-btn");
+const overlayBody = document.querySelector(".overlay-body");
 mobileMenuBtn.addEventListener("click", () => {
   mobileMenu.classList.add("open-mobile-menu");
+  overlayBody.classList.add("overlay-body-enable");
 });
 
 closeBtn.addEventListener("click", () => {
   mobileMenu.classList.remove("open-mobile-menu");
+  overlayBody.classList.remove("overlay-body-enable");
 });
