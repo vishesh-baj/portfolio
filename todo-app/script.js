8911409f749a7a76c304efe5f167f2929838fac7
vishesh-baj <vishesh.bajpayee@icloud.com>
2022-03-18 02:31:23 -0500

    added todo itemconfig

--- todo-app/script.js
+++ todo-app/script.js
@@ -3,6 +3,18 @@
 const closeButton = document.querySelector("#close-menu");
 const openButton = document.querySelector(".open-menu");
 const sideMenu = document.querySelector(".menu");
+const createTodoBtn = document.querySelector(".create-todo-btn");
+const todoContainer = document.querySelector(".todo-wrapper");
+const todosList = [];
+
+const createTodos = () => {
+  const html = ` <div class="todo">
+          <input placeholder="TODO NAME" type="text" />
+          <i class="fa-solid fa-circle-check"></i>
+        </div>`;
+
+  todoContainer.insertAdjacentHTML("afterbegin", html);
+};
 
 openButton.addEventListener("click", () => {
   sideMenu.classList.add("sidemenu-toggler");
@@ -11,3 +23,5 @@ openButton.addEventListener("click", () => {
 closeButton.addEventListener("click", () => {
   sideMenu.classList.remove("sidemenu-toggler");
 });
+
+createTodoBtn.addEventListener("click", createTodos);
