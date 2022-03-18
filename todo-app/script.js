8fcd7fe98077ed337c5a13c8d27a1304a021f55e
vishesh-baj <vishesh.bajpayee@icloud.com>
2022-03-18 00:28:56 -0500

    added todo mobile navigation bar

--- todo-app/script.js
+++ todo-app/script.js
@@ -1,20 +1,13 @@
-const palleteBtn = document.querySelector(".pallette-btn");
-const palleteMenu = document.querySelector(".pallete-menu");
-const closePalleteMenuBtn = document.querySelector(".close-menu-btn");
-// theme buttons
-const themeBtns = document.querySelectorAll(".theme-btn");
-// add todo button
-const addTodo = document.querySelector(".add-todo");
-// plus-btn
-const plusBtn = document.querySelector("#add-item");
-palleteBtn.addEventListener("click", () => {
-  palleteMenu.classList.toggle("pallete-menu-toggle");
+"use-strict";
+
+const closeButton = document.querySelector("#close-menu");
+const openButton = document.querySelector(".open-menu");
+const sideMenu = document.querySelector(".menu");
+
+openButton.addEventListener("click", () => {
+  sideMenu.classList.add("sidemenu-toggler");
 });
 
-closePalleteMenuBtn.addEventListener("click", () => {
-  palleteMenu.classList.toggle("pallete-menu-toggle");
-});
-
-addTodo.addEventListener("click", () => {
-  console.log("Clicked");
+closeButton.addEventListener("click", () => {
+  sideMenu.classList.remove("sidemenu-toggler");
 });
