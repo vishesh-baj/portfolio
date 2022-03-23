d006d17c038853cf64d24a225317d91f5069e58b
vishesh-baj <vishesh.bajpayee@icloud.com>
2022-03-23 14:14:42 -0500

    added lazy loading for images

--- feature-rich-website/script.js
+++ feature-rich-website/script.js
@@ -146,7 +146,7 @@ headerObserver.observe(header);
 const allSections = document.querySelectorAll(".section");
 const revealSection = function (entries, observer) {
   const [entry] = entries;
-  console.log(entry);
+  // console.log(entry);
   if (!entry.isIntersecting) return;
   entry.target.classList.remove("section--hidden");
   observer.unobserve(entry.target);
@@ -161,6 +161,24 @@ allSections.forEach((section) => {
   section.classList.add("section--hidden");
 });
 
+// ? LAZY LOADING IMAGES
+const imgTargets = document.querySelectorAll("img[data-src]");
+const loadImg = (entries, observer) => {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  // Replace src attribure with data-src
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener("load", () => {
+    entry.target.classList.remove("lazy-img");
+  });
+  observer.unobserve(entry.target);
+};
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: "200px",
+});
+imgTargets.forEach((img) => imgObserver.observe(img));
 // ///////////////////////////////////////////////////////////////////
 
 // SELECTING CREATING AND DELETING ELEMENTS
