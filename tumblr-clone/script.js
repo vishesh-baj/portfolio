67ce49cea1c2e69c5af0cbdd5cf2f18a4f62cb89
vishesh-baj <vishesh.bajpayee@icloud.com>
2022-03-26 06:01:37 -0500

    added cover and nav

--- tumblr-clone/script.js
+++ tumblr-clone/script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+const profileBtn = document.querySelector(".profile-container");
+const profileContainer = document.querySelector(".profile-popup-container");
+const arrow = document.querySelector("#arrow");
+const hero = document.querySelector(".hero");
+profileBtn.addEventListener("click", () => {
+  profileContainer.classList.toggle("hidden");
+  const isHidden = profileContainer.classList.contains("hidden");
+  arrow.className = "fa-solid";
+  isHidden
+    ? arrow.classList.add("fa-arrow-down")
+    : arrow.classList.add("fa-arrow-up");
+});
+
+hero.addEventListener("click", () => profileContainer.classList.add("hidden"));
