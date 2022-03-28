0d263e8e9afbead8f090213716e99a8f05f31085
vishesh-baj <vishesh.bajpayee@icloud.com>
2022-03-28 04:43:40 -0500

    added todo item functionality in todo app

--- todo-app/script.js
+++ todo-app/script.js
@@ -1,32 +1,24 @@
 "use strict";
 
-const todoInput = document.querySelector("#todo-name");
-const todoSubmit = document.querySelector(".todo-form");
-const todoContainer = document.querySelector(".todos");
-let todos = [];
+const todoInpForm = document.querySelector(".todo-inp");
+const todoInp = document.querySelector("#todo-selection");
+const todoContainer = document.querySelector(".todo-list");
+const todoList = [];
 
-const displayTodos = (todoList) => {
-  todoContainer.innerHTML = "";
-  todoList.forEach((todoItem) => {
-    const html = `<div class="todo">
-              <h3>${todoItem}</h3>
-              <div class="options">
-                <i class="checkced fa-solid fa-check"></i>
-                <i class="delete fa-solid fa-trash-can"></i>
-              </div>
-            </div>`;
-    todoContainer.insertAdjacentHTML("afterbegin", html);
-  });
-};
-
-const addTodo = (todoList) => {
-  todoList.push(todoInput.value);
-  console.log(todos);
-};
-
-todoSubmit.addEventListener("submit", (e) => {
+todoInpForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  addTodo(todos);
-  displayTodos(todos);
-  todoInput.value = "";
+  todoList.push(todoInp.value);
+  console.log(todoList);
+
+  const html = `<div class="todo-item">
+  <h1>${todoInp.value}</h1>
+  <button>Delete</button>
+  </div>`;
+  if (todoInp.value.length === 0) {
+    alert("Cannot add empty value");
+  } else {
+    todoContainer.insertAdjacentHTML("afterbegin", html);
+  }
+
+  todoInp.value = "";
 });
