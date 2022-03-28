d10a762295c44e6aebc6ad98e5eb58419590e290
vishesh-baj <vishesh.bajpayee@icloud.com>
2022-03-27 19:15:31 -0500

    added multiple popups markers

--- map-app/script.js
+++ map-app/script.js
@@ -16,7 +16,34 @@ if (navigator.geolocation) {
     (position) => {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
+      // ** google maps link
       console.log(`http://www.google.com/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude];
+      // ** storing result of creating a map invariable to add event listener
+      const map = L.map("map").setView(coords, 13);
+      // console.log(map);
+      L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      map.on("click", (mapEvent) => {
+        const { lat, lng } = mapEvent.latlng;
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: "running-popup",
+            })
+          )
+          .setPopupContent("Workout")
+          .openPopup();
+      });
     },
     () => {
       alert("Could not get your location");
