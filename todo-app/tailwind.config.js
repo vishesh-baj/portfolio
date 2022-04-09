65f2268bb2a3ff06b2dddf1516ca659d79dc88f7
vishesh-baj <vishesh.bajpayee@icloud.com>
2022-04-09 02:02:29 -0500

    deleted initial html for todo app

---
+++ todo-app/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      },
+    },
+  },
+
+  plugins: [],
+};
