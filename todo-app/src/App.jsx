c0d0e4a65279297d7fa05118f84e7d2cce5cd13c
vishesh-baj <vishesh.bajpayee@icloud.com>
2022-04-20 13:06:53 +0530

    added react todo app initiasl started file

---
+++ todo-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Title } from "./styled-components";
+import TodoContainer from "./TodoContainer";
+const App = () => {
+  return (
+    <React.Fragment>
+      <Title>TODO APP</Title>
+    </React.Fragment>
+  );
+};
+
+export default App;
