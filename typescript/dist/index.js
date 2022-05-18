26633f44c39d77fe20ba236c9a9f5c8547256d17
vishesh-baj <vishesh.bajpayee@icloud.com>
2022-05-18 15:44:09 +0530

    added typescript files in portfolio

---
+++ typescript/dist/index.js
@@ -0,0 +1,19 @@
+"use strict";
+// Basic Types
+let id = 5;
+let company = "Awesome";
+let isPublished = true;
+let x = "Sool";
+x = false;
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, "two", false];
+// tuple
+let tuple = [1, "1", false];
+// tuple array
+let employee;
+employee = [
+    [1, "vishesh"],
+    [2, "cool"],
+    [3, "three"],
+];
+// union 
---
+++ typescript/src/index.ts
@@ -0,0 +1,21 @@
+// Basic Types
+let id: number = 5;
+let company: string = "Awesome";
+let isPublished: boolean = true;
+let x: any = "Sool";
+x = false;
+
+let ids: number[] = [1, 2, 3, 4, 5];
+let arr: any[] = [1, "two", false];
+
+// tuple
+let tuple: [number, string, boolean] = [1, "1", false];
+// tuple array
+let employee: [number, string][];
+employee = [
+  [1, "vishesh"],
+  [2, "cool"],
+  [3, "three"],
+];
+
+// union 
