7154843ddf99db1009c642112109d130d08eae6c
vishesh-baj <vishesh.bajpayee@icloud.com>
2022-05-19 12:07:02 +0530

    added typescript docs

--- typescript/dist/index.js
+++ typescript/dist/index.js
@@ -16,4 +16,20 @@ employee = [
     [2, "cool"],
     [3, "three"],
 ];
-// union 
+// union
+let idz = 2;
+// Enum
+var direction1;
+(function (direction1) {
+    direction1[direction1["Up"] = 0] = "Up";
+    direction1[direction1["Down"] = 1] = "Down";
+    direction1[direction1["Left"] = 2] = "Left";
+    direction1[direction1["Right"] = 3] = "Right";
+})(direction1 || (direction1 = {}));
+const user = {
+    id: 1,
+    name: " vishesh",
+};
+// Type Assertion
+let cid = 1;
+let customerId = cid;
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -18,4 +18,92 @@ employee = [
   [3, "three"],
 ];
 
-// union 
+// union
+let idz: string | number = 2;
+
+// Enum
+enum direction1 {
+  Up,
+  Down,
+  Left,
+  Right,
+}
+
+// Objects
+type User = {
+  id: number;
+  name: string;
+};
+
+const user: User = {
+  id: 1,
+  name: " vishesh",
+};
+
+// Type Assertion
+let cid: any = 1;
+let customerId = <number>cid;
+
+// Types in functions
+function addSum(x: number, y: number): number {
+  return x + y;
+}
+
+function log(message: string | number): void {
+  console.log(message);
+}
+
+// Interface
+interface UserInterface {
+  // Can only read but not write
+  readonly id: number | string;
+  name: string;
+  // age is potional when we put question mark
+  age?: number;
+}
+
+const user1: UserInterface = {
+  id: "23d",
+  name: "Vishesh",
+  age: 23,
+};
+
+// Interface with a function
+interface MathFunc {
+  (x: number, y: number): number;
+}
+const add: MathFunc = (x: number, y: number): number => x + y;
+const sub: MathFunc = (x: number, y: number): number => x - y;
+
+// Classes and interface
+interface PersonInterface {
+  // Can only read but not write
+  id: number | string;
+  name: string;
+}
+
+class Person implements PersonInterface {
+  id: number;
+  name: string;
+  constructor(id: number, name: string) {
+    this.id = id;
+    this.name = name;
+  }
+
+  register() {
+    return `${this.name} is now registered`;
+  }
+}
+
+// Subclasses in Typescript
+class Employee extends Person {
+  position: string;
+  constructor(id: number, name: string, position: string) {
+    super(id, name);
+    this.position = position;
+  }
+}
+
+const vishesh = new Person(1, "Vishesh");
+
+// Generics 
