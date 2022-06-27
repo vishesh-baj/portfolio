fcbd835b0d7e603bfa4ce701a140a72ac251fdb3
vishesh_baj <vishesh.bajpayee@icloud.com>
2022-06-28 01:30:52 +0530

    added navbar and video in home page

---
+++ web-vibe/src/components/CoverVideo.jsx
@@ -0,0 +1,135 @@
+import { motion } from "framer-motion";
+import React from "react";
+import styled from "styled-components";
+import MainVideo from "../assets/Walking Girl.mp4";
+const VideoContainer = styled.section`
+  width: 100%;
+  height: 100vh;
+  position: relative;
+
+  video {
+    height: 100vh;
+    width: 100%;
+    object-fit: cover;
+  }
+`;
+
+const DarkOverlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 1;
+  background-color: ${(props) => `rgba(${props.theme.bodyRgba},0.6)`};
+`;
+
+const Title = styled(motion.div)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 5;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: ${(props) => props.theme.text};
+
+  h1 {
+    font-family: "Kaushan Script";
+    font-size: ${(props) => props.theme.fontBig};
+    text-shadow: 1px 1px 1px ${(props) => props.theme.body};
+  }
+
+  h2 {
+    font-family: "Sirin Stencil";
+    font-size: ${(props) => props.theme.fontlg};
+    font-weight: 300;
+    text-shadow: 1px 1px 1px ${(props) => props.theme.body};
+    text-transform: capitalize;
+  }
+
+  div {
+    display: flex;
+    flex-direction: row;
+  }
+`;
+
+const container = {
+  hidden: {
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+    transition: {
+      duration: 2,
+      ease: "easeInOut",
+    },
+  },
+};
+
+const item = {
+  hidden: {
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+    transition: {
+      delayChildren: 2,
+      staggerChildren: 0.3,
+    },
+  },
+};
+
+const CoverVideo = () => {
+  return (
+    <VideoContainer>
+      <DarkOverlay />
+      <Title variants={container} initial="hidden" animate="show">
+        <div>
+          <motion.h1
+            variants={item}
+            data-scroll
+            data-scroll-delay="0.14"
+            data-scroll-speed="4"
+          >
+            W
+          </motion.h1>
+          <motion.h1
+            variants={item}
+            data-scroll
+            data-scroll-delay="0.09"
+            data-scroll-speed="4"
+          >
+            i
+          </motion.h1>
+          <motion.h1
+            variants={item}
+            data-scroll
+            data-scroll-delay="0.06"
+            data-scroll-speed="4"
+          >
+            b
+          </motion.h1>
+          <motion.h1
+            variants={item}
+            data-scroll
+            data-scroll-delay="0.04"
+            data-scroll-speed="4"
+          >
+            e
+          </motion.h1>
+        </div>
+        <h2 data-scroll data-scroll-delay="0.04" data-scroll-speed="2">
+          Inspire. Create. Belive
+        </h2>
+      </Title>
+      <video src={MainVideo} type="video/mp4" autoPlay muted loop />
+    </VideoContainer>
+  );
+};
+
+export default CoverVideo;
---
+++ web-vibe/src/styles/Themes.js
@@ -0,0 +1,20 @@
+// Add themes as per your requirement
+export const dark = {
+  body: "#202020", // black shade
+  text: "#fff",
+  bodyRgba: "32,32,32",
+  textRgba: "255,255,255",
+
+  grey: "#bebebe",
+
+  fontxs: "0.75em", // 1em = 16px
+  fontsm: "0.875",
+  fontmd: "1em",
+  fontlg: "1.25em",
+  fontxl: "2em",
+  fontxxl: "3em",
+  fontxxxl: "5em",
+  fontBig: "10em",
+
+  navHeight: "5rem",
+};
